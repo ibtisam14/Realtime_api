acd9271c8e77393fa6f05bb5fa4fc9e13b6f48e2
ibtisam14 <ibtsamulhaq14@gmail.com>
2025-09-26 11:53:31 +0500

    realtime api code

---
+++ realtime_api/asgi.py
@@ -0,0 +1,18 @@
+# realtime_project/asgi.py
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime_api.settings")
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
